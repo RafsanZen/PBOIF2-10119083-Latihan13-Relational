e2229a1431f51a1b7933375695a9ddaa1a334557
Rafsan Zen M <73044188+RafsanZen@users.noreply.github.com>
2020-10-19 19:26:01 +0700

    Add files via upload

---
+++ PBOIF2-10119083-Latihan13-Relational/src/pboif2/pkg10119083/latihan13/relational/PBOIF210119083Latihan13Relational.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119083.latihan13.relational;
+
+/**
+ *
+ * @author 
+ * Nama      : Rafsan Zen Mustaofa
+ * Kelas     : IF2
+ * NIM       : 10119083
+ * Deskripsi : relational
+ */
+public class PBOIF210119083Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int bil1 = 5;
+        int bil2 = 2;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("b >= a = " + (bil2 >= bil1));
+        System.out.println("b <= a = " + (bil2 <= bil1));
+    }
+    
+}
